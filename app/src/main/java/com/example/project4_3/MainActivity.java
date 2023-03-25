4dcc6fc651dfad5c600c615af74821f269fb8c79
ji-min-choi <4metree@naver.com>
2023-03-26 03:33:32 +0900

    오픈소스전문프로젝트 3주차 과제 2

---
+++ app/src/main/java/com/example/project4_3/MainActivity.java
@@ -0,0 +1,143 @@
+package com.example.project4_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+//2021041083 최지민
+public class MainActivity extends AppCompatActivity {
+    EditText edit1, edit2;
+    Button btnAdd, btnSub, btnMul, btnDiv, btnRem;
+    TextView textResult;
+    String num1, num2;
+    Float result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("초간단 계산기(수정)");
+
+        edit1 = (EditText) findViewById(R.id.Edit1);
+        edit2 = (EditText) findViewById(R.id.Edit2);
+
+        btnAdd = (Button) findViewById(R.id.BtnAdd);
+        btnSub = (Button) findViewById(R.id.BtnSub);
+        btnMul = (Button) findViewById(R.id.BtnMul);
+        btnDiv = (Button) findViewById(R.id.BtnDiv);
+        btnRem = (Button) findViewById(R.id.BtnRem);
+
+        textResult = (TextView) findViewById(R.id.TextResult);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+
+                if(num1.trim().equals("")||num2 .trim().equals(""))
+                {
+                    Toast myToast = Toast.makeText(getApplicationContext(),"값을 입력하세요.",Toast.LENGTH_SHORT);
+                    myToast.show();
+                }
+                else
+                {
+                    result = Float.parseFloat(num1) + Float.parseFloat(num2);
+                    textResult.setText("계산 결과 : "+ result.toString());
+                }
+            }
+        });
+
+        btnSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+
+                if(num1.trim().equals("")||num2 .trim().equals(""))
+                {
+                    Toast myToast = Toast.makeText(getApplicationContext(),"값을 입력하세요.",Toast.LENGTH_SHORT);
+                    myToast.show();
+                }
+                else
+                {
+                    result = Float.parseFloat(num1) - Float.parseFloat(num2);
+                    textResult.setText("계산 결과 : "+ result.toString());
+                }
+            }
+        });
+
+        btnMul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+
+                if(num1.trim().equals("")||num2 .trim().equals(""))
+                {
+                    Toast myToast = Toast.makeText(getApplicationContext(),"값을 입력하세요.",Toast.LENGTH_SHORT);
+                    myToast.show();
+                }
+                else
+                {
+                    result = Float.parseFloat(num1) * Float.parseFloat(num2);
+                    textResult.setText("계산 결과 : "+ result.toString());
+                }
+            }
+        });
+
+        btnDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+
+                if(num1.trim().equals("")||num2 .trim().equals(""))
+                {
+                    Toast myToast = Toast.makeText(getApplicationContext(),"값을 입력하세요.",Toast.LENGTH_SHORT);
+                    myToast.show();
+                }
+                else if(num2.trim().equals("0"))
+                {
+                    Toast myToast = Toast.makeText(getApplicationContext(),"0으로 나눌 수 없습니다.",Toast.LENGTH_SHORT);
+                    myToast.show();
+                }
+                else
+                {
+                    result = Float.parseFloat(num1) / Float.parseFloat(num2);
+                    textResult.setText("계산 결과 : "+ result.toString());
+                }
+            }
+        });
+
+        btnRem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+
+                if(num1.trim().equals("")||num2 .trim().equals(""))
+                {
+                    Toast myToast = Toast.makeText(getApplicationContext(),"값을 입력하세요.",Toast.LENGTH_SHORT);
+                    myToast.show();
+                }
+                else if(num2.trim().equals("0"))
+                {
+                    Toast myToast = Toast.makeText(getApplicationContext(),"0으로 나눌 수 없습니다.",Toast.LENGTH_SHORT);
+                    myToast.show();
+                }
+                else
+                {
+                    result = Float.parseFloat(num1) % Float.parseFloat(num2);
+                    textResult.setText("계산 결과 : "+ result.toString());
+                }
+            }
+        });
+    }
+}
